300c0a2f50f5a4c2857d9d91ca28802642fbe6aa
Abhishek Yadav <abhishekyadav@Abhisheks-MacBook-Pro.local>
2023-11-24 22:20:54 +0530

    created signup page and init trpc

---
+++ src/lib/validators/accountCredentialsValidator.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+
+export const AuthCredentialValidator = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Password must be 8 characters long"})
+})
+
+export type TAuthCredentialsValidator = z.infer<typeof AuthCredentialValidator>
