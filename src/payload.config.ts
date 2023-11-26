1f1df6ed80627f65e29c9054c5576cae21cc0ba6
Abhishek Yadav <abhishekyadav@Abhisheks-MacBook-Pro.local>
2023-11-27 00:12:27 +0530

    created and added collections in payload

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,18 +1,22 @@
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { slateEditor } from '@payloadcms/richtext-slate'
-import dotenv from 'dotenv'
-import path from 'path'
-import { buildConfig } from 'payload/config'
-import { Users } from './collections/Users'
+import { webpackBundler } from '@payloadcms/bundler-webpack';
+import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import dotenv from 'dotenv';
+import path from 'path';
+import { buildConfig } from 'payload/config';
+import { Users } from './collections/Users';
+import { Products } from './collections/Products/Products';
+import { Media } from './collections/Media';
+import { ProductFile } from './collections/ProductFile';
+import { Orders } from './collections/Orders';
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
-})
+});
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users], 
+  collections: [Users, Products, Media, ProductFile, Orders],
   routes: {
     admin: '/sell',
   },
@@ -35,4 +39,4 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
-})
+});
