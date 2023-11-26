ce12b0024c9940269f2791d52731d1845339fe39
Abhishek Yadav <abhishekyadav@Abhisheks-MacBook-Pro.local>
2023-11-26 20:53:39 +0530

    completed auth flow

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,5 +1,6 @@
+import { ExpressContext } from "@/server";
 import { initTRPC } from "@trpc/server";
 
-export const t = initTRPC.context().create()
+export const t = initTRPC.context<ExpressContext>().create()
 export const router = t.router
 export const publicProcedure = t.procedure
