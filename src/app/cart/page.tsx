4e07cbf5a3ce60b378885adb65141a84a07c859d
Abhishek Yadav <abhishekyadav@Abhisheks-MacBook-Pro.local>
2023-11-29 17:26:08 +0530

    integrated stripe

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button';
 import { PRODUCT_CATEGORIES } from '@/config';
 import { useCart } from '@/hooks/useCart';
 import { cn, formatPrice } from '@/lib/utils';
+import { trpc } from '@/trpc/client';
 import { Check, Loader2, X } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -14,6 +15,15 @@ const page = () => {
   const { items, removeItem } = useCart();
 
   const router = useRouter();
+
+  const { mutate: createCheckoutSession, isLoading } =
+    trpc.payment.createSession.useMutation({
+      onSuccess: ({ url }) => {
+        if (url) router.push(url);
+      },
+    });
+
+  const productIds = items.map(({ product }) => product.id);
 
   const [isMounted, setIsMounted] = useState<boolean>(false);
   useEffect(() => {
@@ -185,7 +195,14 @@ const page = () => {
             </div>
 
             <div className="mt-6">
-              <Button className="w-full" size="lg">
+              <Button
+                onClick={() => createCheckoutSession({ productIds })}
+                className="w-full"
+                size="lg"
+              >
+                {isLoading ? (
+                  <Loader2 className="w-4 h-4 animate-spin mr-1.5" />
+                ) : null}
                 Checkout
               </Button>
             </div>
